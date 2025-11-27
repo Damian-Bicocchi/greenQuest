8f2aea6773c4292d519e4ce8c240f8b1c0ef0016
franns008 <franns008@gmail.com>
2025-11-27 10:51:28 -0300

    bottom navigation andando! hay que adaptarlo al proyecto

--- app/src/main/java/com/example/greenquest/MenuPrincipal.kt
+++ app/src/main/java/com/example/greenquest/MenuPrincipal.kt
@@ -1,23 +1,56 @@
 package com.example.greenquest
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import androidx.fragment.app.Fragment
+import com.example.greenquest.databinding.ActivityToolbarBinding
 class MenuPrincipal : AppCompatActivity() {
 
+    private val firstFragment = FirstFragment()
+    private val secondFragment = SecondFragment()
+    private val thirdFragment = ThirdFragment()
 
+    private lateinit var binding: ActivityToolbarBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_menu_principal)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityToolbarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navigation = binding.bottomNavigation
+        navigation.setOnItemSelectedListener { item -> onNavigationItemSelectedListener(item) }
+        loadFragment(firstFragment)
+    }
+
+    private val onNavigationItemSelectedListener: (MenuItem) -> Boolean = { item ->
+        when (item.itemId) {
+            R.id.firstFragment -> {
+                loadFragment(firstFragment)
+                true
+            }
+
+            R.id.secondFragment -> {
+                loadFragment(secondFragment)
+                true
+            }
+
+            R.id.thirdFragment -> {
+                loadFragment(thirdFragment)
+                true
+            }
+
+            else -> false
         }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_container, fragment)
+        transaction.commit()
     }
 }
