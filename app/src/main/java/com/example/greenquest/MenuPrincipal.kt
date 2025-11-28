5f07b089e04dd3afe2285127aff8269350e5af71
franns008 <franns008@gmail.com>
2025-11-28 10:20:42 -0300

    bottom navigation andando y adaptado al proyecto

--- app/src/main/java/com/example/greenquest/MenuPrincipal.kt
+++ app/src/main/java/com/example/greenquest/MenuPrincipal.kt
@@ -11,9 +11,13 @@ import androidx.fragment.app.Fragment
 import com.example.greenquest.databinding.ActivityToolbarBinding
 class MenuPrincipal : AppCompatActivity() {
 
-    private val firstFragment = FirstFragment()
-    private val secondFragment = SecondFragment()
-    private val thirdFragment = ThirdFragment()
+    private val topGlobalFragment = TopGlobalFragment()
+    private val TopAmigosFragment = TopAmigosFragment()
+    private val escanearFragment = EscanearFragment()
+
+    private val categorizarFragment = CategorizarFragment()
+
+    private val amigosFragment = AmigosFragment()
 
     private lateinit var binding: ActivityToolbarBinding
 
@@ -24,23 +28,32 @@ class MenuPrincipal : AppCompatActivity() {
 
         val navigation = binding.bottomNavigation
         navigation.setOnItemSelectedListener { item -> onNavigationItemSelectedListener(item) }
-        loadFragment(firstFragment)
+        loadFragment(topGlobalFragment)
     }
 
     private val onNavigationItemSelectedListener: (MenuItem) -> Boolean = { item ->
         when (item.itemId) {
-            R.id.firstFragment -> {
-                loadFragment(firstFragment)
+            R.id.topGlobalFragment -> {
+                loadFragment(topGlobalFragment)
                 true
             }
 
-            R.id.secondFragment -> {
-                loadFragment(secondFragment)
+            R.id.topAmigosFragment -> {
+                loadFragment(TopAmigosFragment)
                 true
             }
 
-            R.id.thirdFragment -> {
-                loadFragment(thirdFragment)
+            R.id.escanearFragment -> {
+                loadFragment(escanearFragment)
+                true
+            }
+
+            R.id.categorizarFragment-> {
+                loadFragment(categorizarFragment)
+                true
+            }
+            R.id.amigosFragment -> {
+                loadFragment(amigosFragment)
                 true
             }
 
