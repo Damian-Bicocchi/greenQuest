6ba781f1c4874285e8396852ff9439aba6c0da51
franns008 <franns008@gmail.com>
2025-11-28 10:20:42 -0300

    bottom navigation andando y adaptado al proyecto

--- app/src/main/java/com/example/greenquest/TopAmigosFragment.kt
+++ app/src/main/java/com/example/greenquest/TopAmigosFragment.kt
@@ -16,7 +16,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [SecondFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SecondFragment : Fragment() {
+class TopAmigosFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_top_amigos, container, false)
     }
 
     companion object {
@@ -49,7 +49,7 @@ class SecondFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SecondFragment().apply {
+            TopAmigosFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
