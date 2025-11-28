d32e2d4c62d0d3536957589ee70882e708440aa6
Damián Bicocchi <pdamianbicocchi@gmail.com>
2025-11-28 16:25:29 -0300

    view restablecer contrasenia

---
+++ app/src/main/java/com/example/greenquest/restablecer_contrasenia.kt
@@ -0,0 +1,20 @@
+package com.example.greenquest
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class restablecer_contrasenia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_restablecer_contrasenia)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
