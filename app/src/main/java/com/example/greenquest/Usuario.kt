9e425434e07e7d5b13c5b9d6cf8fabd5ac5065e1
franns008 <franns008@gmail.com>
2025-11-28 23:26:45 -0300

    Se crea el top por amigos

---
+++ app/src/main/java/com/example/greenquest/Usuario.kt
@@ -0,0 +1,4 @@
+package com.example.greenquest
+
+class Usuario(val nombre: String, val puntos: Int, val image: String) {
+}
--- app/src/main/java/com/example/greenquest/fragments/TopAmigosFragment.kt
+++ app/src/main/java/com/example/greenquest/fragments/TopAmigosFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.greenquest.AdapterUsuarios
 import com.example.greenquest.R
+import com.example.greenquest.Usuario
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,39 +26,30 @@ class TopAmigosFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val usuarios = listOf(
+        Usuario("Amigo1", 150,"none"),
+        Usuario("Amigo2", 120,"none"),
+        Usuario("Amigo3", 100,"none"),
+        Usuario("Amigo4", 80,"none"),
+        Usuario("Amigo5", 60,"none"),
+        Usuario("Amigo6", 50,"none")
+    )
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_top_amigos, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FifthFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TopAmigosFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerTopAmigos)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = AdapterUsuarios(usuarios)
     }
+
+
 }
