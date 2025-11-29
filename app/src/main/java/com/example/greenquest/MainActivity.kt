dfe1fdbab043f76c8118d66d072929443e3cddcb
Damián Bicocchi <pdamianbicocchi@gmail.com>
2025-11-29 00:44:03 -0300

    Hay un flujo al menos en recuperar contraseña

--- app/src/main/java/com/example/greenquest/MainActivity.kt
+++ app/src/main/java/com/example/greenquest/MainActivity.kt
@@ -1,38 +1,30 @@
 package com.example.greenquest
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewScreenSizes
-import com.example.greenquest.ui.theme.GreenQuestTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_iniciar_sesion)
+
+
         val iniciarSesion = findViewById<View>(R.id.iniciar_sesion_button)
         iniciarSesion.setOnClickListener {
+            // Aquí va la lógica de login
+        }
+
+        val linkRecuperar = findViewById<TextView>(R.id.link_recuperar_contrasenia)
+        linkRecuperar.setOnClickListener {
+            val intent = Intent(this, recuperar_contrasenia::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/greenquest/recuperar_contrasenia.kt
+++ app/src/main/java/com/example/greenquest/recuperar_contrasenia.kt
@@ -1,6 +1,10 @@
 package com.example.greenquest
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +16,28 @@ class recuperar_contrasenia : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_recuperar_contrasenia)
 
+        val restablecerContrasenia = findViewById<View>(R.id.btn_recuperar)
+        restablecerContrasenia.setOnClickListener {
+            val inputEmail = findViewById<EditText>(R.id.input_email)
+            val mensajeError = findViewById<TextView>(R.id.mensaje_error)
+
+            val emailTexto = inputEmail.text.toString().trim()
+
+            if (emailTexto.isEmpty()) {
+                mensajeError.visibility = View.VISIBLE
+                mensajeError.text = "Debe ingresar un correo válido"
+            } else {
+                mensajeError.visibility = View.INVISIBLE
+
+                // Redirigimos a la activity
+                val intent = Intent(this, restablecer_contrasenia::class.java)
+                startActivity(intent)
+            }
+        }
+        val cancelar = findViewById<View>(R.id.btn_cancelar)
+        cancelar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
