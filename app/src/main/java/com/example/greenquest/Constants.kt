673945bc74c39b2fcde3da1d962a3b91f857b8af
Damián Bicocchi <pdamianbicocchi@gmail.com>
2025-12-12 17:14:03 -0300

    commit previo a merge

--- app/src/main/java/com/example/greenquest/Constants.kt
+++ app/src/main/java/com/example/greenquest/Constants.kt
@@ -2,7 +2,7 @@ package com.example.greenquest
 
 object Constants {
     const val BASEUREMULADOR = "http://10.0.2.2:8000/"
-    const val BASEURLCELU = "http://192.168.0.8:8000/"
+    const val BASEURLCELU = "http://192.168.1.7:8000/"
 
     const val PATH_API = "api/"
 }
