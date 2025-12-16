5ec440c9d51eed5bb218691e814d9ef89361bc61
Damián Bicocchi <damian.bicocchi211148@alumnos.info.unlp.edu.ar>
2025-12-16 18:50:10 -0300

    Feature scan

--- app/src/main/java/com/example/greenquest/database/DatosEscaneo.kt
+++ app/src/main/java/com/example/greenquest/database/DatosEscaneo.kt
@@ -1,10 +1,12 @@
 package com.example.greenquest.database
 
 import android.os.Parcelable
+import com.example.greenquest.apiParameters.TipoResiduo
 import kotlinx.android.parcel.Parcelize
+
 
 @Parcelize
 data class DatosEscaneo(
-    val tipoResiduo: com.example.greenquest.apiParameters.TipoResiduo,
+    val tipoResiduo: TipoResiduo,
     val puntos: Int,
 ) : Parcelable
