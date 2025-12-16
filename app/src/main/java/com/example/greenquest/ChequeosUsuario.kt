2dc17aad7ee0ded920c48df4c917df10b17f4d74
franns008 <franns008@gmail.com>
2025-12-16 19:18:21 -0300

    se mejora la forma en que se muestran las validaciones

--- app/src/main/java/com/example/greenquest/ChequeosUsuario.kt
+++ app/src/main/java/com/example/greenquest/ChequeosUsuario.kt
@@ -1,11 +1,14 @@
 object ChequeosUsuario {
 
     fun esValidoUsername(username: String): Boolean {
-        val regex = Regex("^[a-zA-Z0-9._-]{3,20}$")
+        val regex = Regex("^[a-zA-Z0-9._-]$")
 
         return regex.matches(username)
     }
 
+    fun esValidoCantidadCaracteresUsername(username: String): Boolean {
+        return username.length in 3..20
+    }
     fun esValidoFormatoContraseña(contraseña: String): Boolean {
         val regex = Regex("^(?=.*[0-9])(?=.*[a-z]*)(?=.*[A-Z]).{8,}$")
         return regex.matches(contraseña)
