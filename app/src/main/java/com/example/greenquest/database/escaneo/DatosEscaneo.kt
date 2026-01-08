2a5257d4097fbafeb1e58513050917be434aaa81
Damián Bicocchi <damian.bicocchi211148@alumnos.info.unlp.edu.ar>
2026-01-08 17:32:40 -0300

    merge feature-trivia > main

--- app/src/main/java/com/example/greenquest/database/escaneo/DatosEscaneo.kt
+++ app/src/main/java/com/example/greenquest/database/escaneo/DatosEscaneo.kt
@@ -2,7 +2,7 @@ package com.example.greenquest.database.escaneo
 
 import android.os.Parcelable
 import com.example.greenquest.apiParameters.TipoResiduo
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class DatosEscaneo(
